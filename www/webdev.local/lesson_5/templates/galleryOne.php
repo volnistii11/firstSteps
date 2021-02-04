3f72dd2b33ad742d064ed80085d0f5c3ecd182a9
volnistii11 <76567146+volnistii11@users.noreply.github.com>
2021-02-04 17:25:27 +0300

    lesson 5 changes

---
+++ www/webdev.local/lesson_5/templates/galleryOne.php
@@ -0,0 +1,12 @@
+<h2>Галерея</h2>
+<div id="main">
+    <div class="post_title"><h2>Моя галерея</h2></div>
+    <div class="gallery">
+        <!-- class="photo" -->
+        <img src="<?= BIG_IMAGES_DIR . $galleryOne['name'] ?>" width="800"
+                height="600"/>
+        <div>
+            <p>Колличество просмотров: <?=$galleryOne['views']?></p>
+        </div>
+    </div>
+</div>
